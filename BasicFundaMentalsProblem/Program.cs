94fd6281407f9b8e6624c90b7c58bded66fe273c
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-16 17:30:35 +0530

    [VINAY] Add: It will Display Today Date and Time

--- BasicFundaMentalsProblem/Program.cs
+++ BasicFundaMentalsProblem/Program.cs
@@ -4,25 +4,15 @@ class Program
 {
     public static void Main(String[] args)
     {
-        int x = 0;
-        x = int.MaxValue;
-        Console.WriteLine(x);
-        x= int.MinValue;
-        Console.WriteLine(x);
-        //Console.WriteLine("Enter the integer value");
-        //uint y = Convert.ToUInt32(Console.ReadLine() ) ;
-        uint y= uint.MinValue;
-        Console.WriteLine(y);
-        Console.WriteLine(typeof(decimal).IsPrimitive);
-        float f_no= 0.0f;
-        double d_no = 0.0d;
-        decimal m_no = 0.0m;
-        f_no = 1 / 5f;
-        d_no = 5 / 75d;
-        m_no = 5 / 105m;
+       DateTime myDate=DateTime.Today;
+        Console.WriteLine(myDate);
+        DateTime myDate1 = DateTime.Now;
+        Console.WriteLine(myDate1);
+            DateTime myDate2 = DateTime.Now;//It will Display TodayDate with the name of month
+        Console.WriteLine(myDate2.ToLongDateString());
+        DateTime myDate3 = DateTime.Now;//It will Display TodayDate with the name of month
+        Console.WriteLine(myDate3.ToLongTimeString());
 
-        Console.WriteLine("float number is:-->{0}",f_no);
-        Console.WriteLine("Decimal number is:-->{0}", m_no );
-        Console.WriteLine(" double number is:-->{0}", m_no);
     }
 }
+
