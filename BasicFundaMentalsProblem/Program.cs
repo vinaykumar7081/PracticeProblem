357751e8208cb23a5fff5d2472ceea2de777072a
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-16 17:39:50 +0530

    [VINAY] Add: It will Display adding the numberof day ,minutes,hours, seconds, milliseconds

--- BasicFundaMentalsProblem/Program.cs
+++ BasicFundaMentalsProblem/Program.cs
@@ -4,14 +4,13 @@ class Program
 {
     public static void Main(String[] args)
     {
-       DateTime myDate=DateTime.Today;
-        Console.WriteLine(myDate);
-        DateTime myDate1 = DateTime.Now;
-        Console.WriteLine(myDate1);
-            DateTime myDate2 = DateTime.Now;//It will Display TodayDate with the name of month
-        Console.WriteLine(myDate2.ToLongDateString());
-        DateTime myDate3 = DateTime.Now;//It will Display TodayDate with the name of month
-        Console.WriteLine(myDate3.ToLongTimeString());
+       DateTime myDate=DateTime.Now;
+        Console.WriteLine(myDate.AddDays(10));
+        Console.WriteLine(myDate.AddDays(-10));
+        Console.WriteLine(myDate.AddYears(10));
+        Console.WriteLine(myDate.AddTicks(10));
+        Console.WriteLine(myDate.AddSeconds (1000));
+        Console.WriteLine(myDate.ToOADate());
 
     }
 }
