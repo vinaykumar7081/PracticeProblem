6a5a3f054f54528723613415362cfbb5ac19b310
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-17 09:41:31 +0530

    [VINAY] Add:Displaying Date ,Day and Time in Different Format

--- BasicFundaMentalsProblem/Program.cs
+++ BasicFundaMentalsProblem/Program.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using BasicFundaMentalsProblem;
 class Program
-{
+{ 
     public static void Main(String[] args)
     {
-        DateTime myDate = DateTime.Now;
-        string formattedDate = string.Format("Date is {0:yyyy MM dddd HH mm ss tt}", myDate);
-        Console.WriteLine(formattedDate);
-        //o/p: Date is 2022 04 Saturday 17 46 46
+        DateTime myDate = DateTime.Now ;
+        string formattedDate = " ";
+        //First Format of the Date Displaying. 
+         formattedDate = string.Format("{0:yyyy MM HH:mm:ss:tt}", myDate);
+        Console.WriteLine(formattedDate);//outPut:-> 16-04-2022 17:55:51
+
+        //Second Formate: <ToDay_DayName> of Month <Month_Name> <Current_year>
+        formattedDate = string.Format("{0: dddd 'of Month'  HHHH 'year'  yyyy}",myDate);
+        Console.WriteLine(formattedDate); // output:->Sunday of Month  09 year  2022
+
+        /*Third Format
+         * <ToDay_DayName>
+         * Month <current_MonthName>
+         * year <Current_Year>
+         */
+        formattedDate = string.Format(" {0: 'Day' dddd  \n ' Month'  HHHH  \n  'year'  yyyy}", myDate);
+        Console.WriteLine(formattedDate); /* output:->
+                                           * 
+                                           */
+
+
+
     }
 }
 
